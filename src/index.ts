cabbe049857cfd36ad0376628ec58ec8e2a47c12
KuroiZZ <habiltatar01@gmail.com>
2026-04-04 17:28:21 +0300

    Note controllers created.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import express from "express";
+import { getAllNotes, getNotesById, createNote, updateNote, deleteNote } from "./Controllers/NoteController";
+import { AppDataSource } from "./Database/Connection";
+import { get } from "node:http";
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+AppDataSource.initialize()
+  .then(() => 
+  {
+    app.get("/note/getall", getAllNotes);
+    app.get("/note/getbyid/:userId", getNotesById);
+    app.post("/note/create", createNote);
+    app.put("/note/edit/:noteId", updateNote);
+    app.delete("/note/delete/:noteId", deleteNote);
+
+    app.listen(3000, () => 
+    {
+      console.log("Server running on port 3000");
+    });
+  })
+  .catch((err) => console.error("Database connection failed:", err));
